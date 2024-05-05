cf910021bfcdfa000c67ab587e5471a142da9a5a
Andrea <andrea.fato20@gmail.com>
2024-05-05 11:41:17 +0200

    Backup; bmp images decoder complete

--- example_SD/Core/Inc/sd_ex.h
+++ example_SD/Core/Inc/sd_ex.h
@@ -11,43 +11,46 @@
 /*************** SETTINGS *****************/
 
 // Frame sizes
-#define FRAME_DIM_WD			100
-#define FRAME_DIM_HT			100
+#define FRAME_DIM_WD					100
+#define FRAME_DIM_HT					100
 
 // Frame division factor (in order to save RAM during run time I chose
 // to subdivide each frame in multiple sub-frames)
 // It must be a multiple of 2
-#define FRAME_DIV_FACTOR		2
+#define FRAME_DIV_FACTOR				20
 
 // Number of frames to be skipped ( in order to save processing time )
-#define FRAME_SKIP_FACTOR		0
+#define FRAME_SKIP_FACTOR				0
 
 // File name
-#define FILE_NAME				"sample.mov"
+#define FILE_NAME						"sample9.bmp"
 
 // File header number of bytes (to be skipped)
-#define FILE_HEADER				54					// .bmp = 54B
+#define FILE_HEADER						54					// .bmp = 54B
 
 // Number of bit of depth
-#define COLOUR_DEPTH 			16
+#define COLOUR_DEPTH 					24
 
 /*****************************************/
 
 /*************** FIXED PARAMETERS *****************/
 
 // Display sizes (considering the rotation of the display)
-#define WIDTH					ST7735_HEIGHT
-#define HEIGHT					ST7735_WIDTH
+#define WIDTH							ST7735_HEIGHT
+#define HEIGHT							ST7735_WIDTH
 
 // Number of bytes per pixel
-#define BYTE_PER_PXL			( COLOUR_DEPTH / 8 )
+#define BYTE_PER_PXL					( COLOUR_DEPTH / 8 )
 
 // Frame sizes
-#define FRAME_TOTAL_SIZE		( FRAME_DIM_WD * FRAME_DIM_HT * BYTE_PER_PXL)
-#define FRAME_SECTION_SIZE		( FRAME_TOTAL_SIZE / FRAME_DIV_FACTOR )
+#define FRAME_TOTAL_BYTE_SIZE			( FRAME_DIM_WD * FRAME_DIM_HT * BYTE_PER_PXL)
+#define FRAM_TOTAL_BYTE_16_SIZE			( FRAME_DIM_WD * FRAME_DIM_HT * 2 )
+#define FRAME_SECTION_BYTE_SIZE			( FRAME_DIM_WD * FRAME_DIV_FACTOR * BYTE_PER_PXL )
+#define FRAME_SECTION_BYTE_16_SIZE 		( FRAME_DIM_WD * FRAME_DIV_FACTOR * 2 )
+#define FRAME_SECTION_PXL_SIZE			( FRAME_DIM_WD * FRAME_DIV_FACTOR )
 
 // Max buffer size
-#define MAX_BUFF_RAM 			( FRAME_SECTION_SIZE + 200 )
+#define MAX_BUFF_RAM 					( FRAME_SECTION_BYTE_SIZE + 200 )
 
 /*****************************************/
 
@@ -60,6 +63,15 @@
 
 /*****************************************/
 
+typedef union
+{
+
+	uint16_t *u16Arr;
+	uint8_t *u8Arr;
+
+}doubleFormat;
+
 void sd_process(void);
+void showImageBmp(char *name);
 
 #endif /* INC_SD_EX_H_ */
--- example_SD/Core/Src/sd_ex.c
+++ example_SD/Core/Src/sd_ex.c
@@ -5,6 +5,8 @@
  *      Author: Fato
  */
 
+#include <string.h>
+
 #include "main.h"
 
 #include "sd_ex.h"
@@ -13,8 +15,8 @@
 #include "fatfs.h"
 
 
-static void showVideo(char *name, int wd, int ht, int nl, int skipFr);
 static void ferror_handler(uint8_t error);
+static void depth24To16(doubleFormat *pxArr, uint16_t length);
 
 
 FATFS FatFs; 		//Fatfs handle
@@ -25,7 +27,7 @@ FRESULT fres; 		//Result after operations
 void sd_process(void)
 {
 
-	ST7735_SetRotation(3); // (wd = 160 ; x_start = 0)  and (ht = 128 ; y_start = 0)
+	ST7735_SetRotation(0); // (wd = 160 ; x_start = 0)  and (ht = 128 ; y_start = 0)
 	HAL_Delay(1000);
 	fillScreen(BLACK);
 
@@ -34,7 +36,7 @@ void sd_process(void)
 	if(fres != FR_OK)
 		ferror_handler(ERROR_0_MOUNT);
 
-	showVideo(FILE_NAME, FRAME_DIM_WD, FRAME_DIM_HT, FRAME_DIV_FACTOR, FRAME_SKIP_FACTOR);
+	showImageBmp(FILE_NAME);
 
 }
 
@@ -45,12 +47,15 @@ void sd_process(void)
 // wd,ht - width, height of the video (raw data has no header with such info)
 // nl - num lines read in one operation (nl*wd*2 bytes are loaded)
 // skipFr - num frames to skip
-static void showVideo(char *name, int wd, int ht, int nl, int skipFr)
+void showImageBmp(char *name)
 {
 
-	uint16_t buf[MAX_BUFF_RAM];			// RAM buffer
+	uint8_t buf[MAX_BUFF_RAM];			// RAM buffer
+	doubleFormat pBuf;
 	UINT byteRead;						// Number of bytes elaborated at time
 
+
+	pBuf.u8Arr = buf;					// Pointer to buf in order to convert format from uint8_t to uint16_t
 
 	fres = f_open(&fil, name, FA_READ);
 	if(fres != FR_OK)
@@ -59,38 +64,69 @@ static void showVideo(char *name, int wd, int ht, int nl, int skipFr)
 	// Point to the initial position
 	f_rewind(&fil);
 
-
 	while(!f_eof(&fil))
 	{
 
 		// Skip header file
 		f_lseek(&fil, fil.fptr + FILE_HEADER);
 
-		for(int i = 0 ; i < ( ht / nl ) ; i++)
+		for(int i = 0 ; i < ( FRAME_DIM_HT / FRAME_DIV_FACTOR ) ; i++)
 		{
 
-			fres = f_read(&fil, buf, ( wd * BYTE_PER_PXL * nl ), &byteRead);
+			//HAL_Delay(10);
+
+			memset(buf, 0, MAX_BUFF_RAM);
+			fres = f_read(&fil, buf, FRAME_SECTION_BYTE_SIZE, &byteRead);
 
 			if(fres != FR_OK)
 				ferror_handler(ERROR_2_READ);
 
-			if(byteRead != FRAME_SECTION_SIZE)
+			if(byteRead != FRAME_SECTION_BYTE_SIZE)
 				ferror_handler(ERROR_3_READ);
 
-			for(int j = 0 ; j < nl ;j++)
-				ST7735_DrawImage(0, ( ( i * nl ) + j ), wd, 1, ( &buf[( j * wd )] ));
+			if(COLOUR_DEPTH == 24)
+				depth24To16(&pBuf, FRAME_SECTION_PXL_SIZE);
+
+			for(int j = 0 ; j < FRAME_DIV_FACTOR ;j++)
+				ST7735_DrawImage(0, ( ( i * FRAME_DIV_FACTOR ) + j ), FRAME_DIM_WD, 1, ( &pBuf.u16Arr[( j * FRAME_DIM_WD )] ));
 
 		}
 
 		// Skip a number of frames
-		if(skipFr > 0)
-			f_lseek(&fil, fil.fptr + ( wd * ht * BYTE_PER_PXL * skipFr ));
+		if(FRAME_SKIP_FACTOR > 0)
+			f_lseek(&fil, fil.fptr + ( FRAME_TOTAL_BYTE_SIZE * FRAME_SKIP_FACTOR ));
 
 	}
+
+	HAL_Delay(10);
 
 	f_close(&fil);
 
 }
+
+
+static void depth24To16(doubleFormat *pxArr, uint16_t length)
+{
+
+	uint8_t b;
+	uint8_t g;
+	uint8_t r;
+
+
+	for(int i = 0 ; i < length ; i++)
+	{
+
+		b = pxArr->u8Arr[i*BYTE_PER_PXL];
+		g = pxArr->u8Arr[i*BYTE_PER_PXL+1];
+		r = pxArr->u8Arr[i*BYTE_PER_PXL+2];
+
+		pxArr->u16Arr[i] = color565(r, g, b);
+		pxArr->u16Arr[i] = ( ( ( pxArr->u16Arr[i] & 0x00ff ) << 8 ) | (( pxArr->u16Arr[i] & 0xff00 ) >> 8) );
+
+	}
+
+}
+
 
 static void ferror_handler(uint8_t error)
 {
